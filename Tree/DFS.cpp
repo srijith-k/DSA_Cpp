54b414dbe306f456d0730a8d32047043b288b10e
srijithk <srijithk.2008@gmail.com>
2024-06-04 20:20:47 +0530

    Tree DFS added

---
+++ Tree/DFS.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+void dfs(TreeNode* root)
+{
+    if(root==NULL)
+        return;
+    cout<<root->val<<" ";
+    dfs(root->left);
+    dfs(root->right);
+}
+
+ int main()
+ {
+     TreeNode* T= new TreeNode(3);
+     T->left= new TreeNode(9);
+     T->right= new TreeNode(20);
+     T->right->left= new TreeNode(15);
+     T->right->right= new TreeNode(7);
+     dfs(T);
+ }
